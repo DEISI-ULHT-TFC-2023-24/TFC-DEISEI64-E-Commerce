872958f9dd03b64c84ac293969833381d8ce7c5d
Shadow <jfserralha@gmail.com>
2023-12-11 14:53:49 +0000

    Added Rate Products Reimbursed and Rate Purchases with no ClientID

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ for customer_id, group_data in customers_grouped:
 
 # Filter the DataFrame where 'Quantity' is greater than 0 (531285) and CustomerID is not null
 filtered_data_quantity = datasetRetail[(datasetRetail['Quantity'] > 0) & (datasetRetail['CustomerID'].isna() == False)]
-print(filtered_data_quantity) # 397924
+#print(filtered_data_quantity) #397924
 
 # Check if any rows satisfy the condition
 if not filtered_data_quantity.empty:
@@ -46,7 +46,7 @@ if not filtered_data_quantity.empty:
 
 # Counts the different clients through the dataset
 category_counts_customerID = datasetRetail['CustomerID'].dropna().value_counts()
-#print(category_counts_customerID)
+#print(category_counts_customerID) #4372
 
 #----------------- Distinct Purcheses Done -----------------#
 
@@ -63,10 +63,22 @@ category_counts_invoiceNo = filtered_data_quantity['InvoiceNo'].value_counts()
 
 #---------------- Products reimbursed ----------------#
 
-purchases_reimbursed = len(datasetRetail['InvoiceNo']) - len(filtered_data_quantity['InvoiceNo'])
-#print(purchases_reimbursed) #10624
+filtered_data_InvoiceNo = datasetRetail[(datasetRetail['Quantity'] > 0)]
+
+products_reimbursed = len(datasetRetail['InvoiceNo']) - len(filtered_data_InvoiceNo['InvoiceNo'])
+#print(products_reimbursed) #10624
 
 #---------------- Purchases with no client ID ----------------#
 
 empty_values_count = datasetRetail['CustomerID'].isna().sum()
 #print(empty_values_count) #135080
+
+#---------------- Rate Products reimbursed ----------------#
+
+reimbursement_rate = (products_reimbursed / len(datasetRetail['InvoiceNo'])) * 100
+#print(reimbursement_rate) #1.9604767590130447
+
+#---------------- Rate Purchases with no ClientID ----------------#
+
+noClientID_rate = (empty_values_count / len(datasetRetail['CustomerID'])) * 100
+#print(noClientID_rate) #24.926694334288598
